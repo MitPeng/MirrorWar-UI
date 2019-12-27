923bd0768712b6e759fe2dcbcf3ca65ff9bbf92a
Mit <719505697@qq.com>
2019-12-27 20:33:42 +0800

    双方计分显示

--- panorama/scripts/custom_game/mirror_war_hud.js
+++ panorama/scripts/custom_game/mirror_war_hud.js
@@ -1,8 +1,11 @@
 "use strict";
 function ShowScore(event) {
-    $.Msg("Hero : ", event.hero_name, " score : ", event.hero_score);
     $("#hero_name").text = event.hero_name;
     $("#hero_score").text = event.hero_score;
+    $("#good_team").text = event.good_team;
+    $("#good_score").text = event.good_score;
+    $("#bad_team").text = event.bad_team;
+    $("#bad_score").text = event.bad_score;
 }
 
 (function () {
