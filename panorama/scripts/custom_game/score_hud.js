6c25899c22364c2a4dbe1efb8ad2fc9ac056e6a1
Mit <719505697@qq.com>
2020-01-30 15:09:06 +0800

    去除红线

--- panorama/scripts/custom_game/score_hud.js
+++ panorama/scripts/custom_game/score_hud.js
@@ -1,7 +1,7 @@
 "use strict";
 function ShowScore(event) {
-    $("#hero_name").text = event.hero_name;
-    $("#hero_score").text = event.hero_score;
+    // $("#hero_name").text = event.hero_name;
+    // $("#hero_score").text = event.hero_score;
     $("#good_team").text = event.good_team;
     $("#good_score").text = event.good_score;
     $("#bad_team").text = event.bad_team;
